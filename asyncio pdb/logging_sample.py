70cfaa06b0f4c0daafe93c9dbb4a348224582029
SHAHAD <shahadmohammed1111@gmail.com>
2025-08-07 14:55:52 +0300

    test files for logging 2

--- asyncio pdb/logging_sample.py
+++ asyncio pdb/logging_sample.py
@@ -1,1 +1,12 @@
 import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+#create a logger`
+logger = logging.getLogger(__name__)
+
+#logging messages
+logger.debug("This is a debug message")
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+logger.critical("This is a critical message")
